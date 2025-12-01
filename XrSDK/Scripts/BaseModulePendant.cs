03900e9a70119513ee3a3078f5fc737e7f53b2d0
DESKTOP-K37NBRN\Administrator <647486848@qq.com>
2025-12-01 19:54:11 +0800

    XrSDK功能文件夹提交

---
+++ XrSDK/Scripts/BaseModulePendant.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace XrSDK
+{
+    //模块挂件基类
+    public abstract class BaseModulePendant : ScriptableObject
+    {
+        [HideInInspector]
+        [SerializeField]
+        protected string moduleName;
+        //运行时创建模块
+        public abstract void CreateModule();
+    }
+}
---
+++ XrSDK/Scripts/RegisterModuleAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace XrSDK
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+    public class RegisterModuleAttribute : Attribute
+    {
+        public string Path;
+        public bool Core;
+
+        public RegisterModuleAttribute(string path, bool core = false)
+        {
+            Path = path;
+            Core = core;
+        }
+    }
+}
